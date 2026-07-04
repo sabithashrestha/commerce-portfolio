ca32bd18451f99a2797c56b8b07b90f3550d4231
sabithashrestha <sabithashrestha@gmail.com>
2026-07-04 15:43:47 +1000

    feat: implement product fetching from Supabase and handle errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
 import supabase from "../src/config/supabaseClient";
 
 const App = () => {
-  console.log("Supabase client:", supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [product, setProduct] = useState(null);
 
-  return <div>App.... Lets try extracting data from supabase to react app</div>;
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data, error } = await supabase.from("products").select();
+
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setProduct(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setProduct(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  return (
+    <div>
+      {fetchError && <p>{fetchError}</p>}
+      {product && (
+        <ul>
+          {product.map((product) => (
+            <li key={product.id}>{product.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default App;
